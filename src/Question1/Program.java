97b70e2563302c19472df65a88488799c11e8beb
Luka <luanafernandes73@gmail.com>
2020-08-16 20:00:49 -0400

    Adicionando anotacoes

--- src/Question1/Program.java
+++ src/Question1/Program.java
@@ -2,39 +2,45 @@ package Question1;
 
 import java.util.*;
 
-public class Program {
-    public static void main(String[] args) {
-        List<String> result = sortLetters();
-        for (String s : result)
-            System.out.println(s);
+public class Program { //classe!
+    public static void main(String[] args) { //main --> static
+        List<String> result = sortLetters(); //chamada, executar
+        for (String word : result)
+            System.out.println(word);
 
-        System.out.println("Total: " + result.size());
+        System.out.println("Total: " + result.size()); //quantos elementos tem na Lista
     }
 
-    private static List<String> sortLetters() {
-        Character[] arrayi = {'a', 'b', 'c', 'd'};
-        Character[] arrayf = new Character[4];
-        Set<String> strings = new HashSet<>();
+    //char --> 1 caracter { 'a'  'u'  '7' '&' '/' }
+    //string --> n caracter (cadeira de caracteres) --> array (vetores e matrizes)
+    // "Pedro" --> 'P' + 'e' + 'd' + 'r' + 'o'
 
-        for (Character v1 : arrayi) {
-            arrayf[0] = v1;
-            for (Character v2 : arrayi) {
-                arrayf[1] = (arrayf[0] == 'b') ? 'a' : v2;
-                for (Character v3 : arrayi) {
-                    arrayf[2] = (arrayf[1] == 'b') ? 'a' : v3;
-                    for (Character v4 : arrayi) {
-                        if (v4 != 'b') {
-                            arrayf[3] = (arrayf[2] == 'b') ? 'a' : v4;
-                            String word = arrayf[0].toString() + arrayf[1].toString() + arrayf[2].toString() + arrayf[3].toString();
-                            if(!(word.contains("a") && word.contains("d")))
-                                strings.add(word);
+    private static List<String> sortLetters() { //Estruturas de dados otimizadas (Algoritmos Complexos)
+        Character[] inicio = {'a', 'b', 'c', 'd'}; //char e string
+        Character[] fim = new Character[4];
+        Set<String> strings = new HashSet<>(); //elimina as redundâncias
+
+        //foreach --> for (int i = 0; i < inicio.length; i++)
+        for (Character v0 : inicio) { //a
+            fim[0] = v0;
+            for (Character v1 : inicio) {
+                fim[1] = (fim[0] == 'b') ? 'a' : v1;
+                for (Character v2 : inicio) {
+                    fim[2] = (fim[1] == 'b') ? 'a' : v2;
+                    for (Character v3 : inicio) {  // a, c, d
+                        if (v3 != 'b') {
+                            fim[3] = (fim[2] == 'b') ? 'a' : v3; //fim [__, __, c, a]
+                            String word = fim[0].toString() + fim[1].toString() + fim[2].toString() + fim[3].toString();
+                            if (!(word.contains("a") && word.contains("d"))) //! --> not
+                                strings.add(word); //add no set as palavras
                         }
                     }
                 }
             }
         }
-        List<String> sortedList = new ArrayList<>(strings);
-        Collections.sort(sortedList);
+
+        List<String> sortedList = new ArrayList<>(strings); //ordem lexicografica
+        Collections.sort(sortedList); //sort --> ordena em ordem lexicografica
         return sortedList;
     }
 }
