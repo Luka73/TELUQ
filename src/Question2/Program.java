43f5d552986e0d0a49df49a75bedce47f3a239c0
Luka <luanafernandes73@gmail.com>
2020-08-25 12:39:28 -0400

    creating transposed matrix

--- src/Question2/Program.java
+++ src/Question2/Program.java
@@ -1,33 +1,21 @@
 package Question2;
 
 public class Program {
-    public static void main(String[] args) {
-        int x, y, z;
-        String msg = "";
-        int[][] vMatrix = new int[9][9];
+    public static void main(String[] args) throws Exception {
         String data = Utils.readFile("C:\\Projects\\TELUQ\\src\\Question2\\resources\\part1.txt");
-        String[] xyz = data.split(" ");
-        if(xyz.length == 81) {
-            for (String s : xyz) {
-                if(s.length() == 3) {
-                    x = Character.getNumericValue(s.charAt(0));
-                    y = Character.getNumericValue(s.charAt(1));
-                    z = Character.getNumericValue(s.charAt(2));
-
-                    vMatrix[x][y] = z;
-                } else { msg = "Invalid data"; }
+        Sudoku sudoku = new Sudoku(data);
+        int[][] tMatrix = new int[9][9];
+        for(int i = 0; i < tMatrix.length; i++) {
+            for (int j = 0; j < tMatrix[i].length; j++) {
+                tMatrix[i][j] = sudoku.matrix[j][i];
             }
-        } else { msg = "Invalid data"; }
-
-
-        if(msg.isEmpty()) {
-            for (int[] matrix : vMatrix) {
-                for (int j = 0; j < vMatrix[0].length; j++) {
-                    System.out.println(matrix[j]);
-                }
-            }
+        }
+        if(sudoku.validMatrix()) {
+            sudoku.showMatrix(sudoku.getMatrix());
+            System.out.println("*********************");
+            sudoku.showMatrix(tMatrix);
         } else {
-            System.out.println(msg);
+            throw new Exception("Inavalid Sudoku");
         }
     }
 }
--- src/Question2/Sudoku.java
+++ src/Question2/Sudoku.java
@@ -1,15 +1,13 @@
 package Question2;
 
 public class Sudoku {
-    private int[][] matrix;
+    public int[][] matrix = new int[9][9];
+    private int[][] transposedMatrix = new int[9][9];
+    private String[] xyz;
 
     public Sudoku(String data) {
-        //this.matrix = matrix;
+        xyz = data.split(" ");
     }
-
-   /* public Sudoku(File file) {
-        this.matrix = matrix;
-    }*/
 
     public int[][] getMatrix() {
         return matrix;
@@ -19,9 +17,42 @@ public class Sudoku {
         this.matrix = matrix;
     }
 
-    private boolean validMatrix(String data) {
+    public boolean validMatrix() {
+        int x, y, z;
+        if(xyz.length != 81) return false;
 
+        for (String s : xyz) {
+            if(s.length() != 3) return false;
+            x = Character.getNumericValue(s.charAt(0));
+            y = Character.getNumericValue(s.charAt(1));
+            z = Character.getNumericValue(s.charAt(2));
 
-        return false;
+            if(x == 9 || y == 9 || z == 0) return false;
+
+            matrix[x][y] = z;
+        }
+
+        return true;
+    }
+
+    public void showMatrix(int[][] matrix) {
+        for(int i = 0; i < matrix.length; i++) {
+            for (int j = 0; j < matrix[i].length; j++) {
+                System.out.print(matrix[i][j] + "\t");
+            }
+            System.out.println();
+        }
+    }
+
+    public void setTransposedMatrix() {
+        for(int i = 0; i < transposedMatrix.length; i++) {
+            for (int j = 0; j < transposedMatrix[i].length; j++) {
+                transposedMatrix[i][j] = matrix[j][i];
+            }
+        }
+    }
+
+    public int[][] getTransposedMatrix() {
+        return transposedMatrix;
     }
 }
