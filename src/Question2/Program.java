3ab3edfbe3685934d604462508fb52d2c69da31e
Luka <luanafernandes73@gmail.com>
2020-08-25 15:04:39 -0400

    finishing Sudoku validation

--- src/Question2/Program.java
+++ src/Question2/Program.java
@@ -2,8 +2,9 @@ package Question2;
 
 public class Program {
     public static void main(String[] args) throws Exception {
-        String data = Utils.readFile("C:\\Projects\\TELUQ\\src\\Question2\\resources\\part1.txt");
+        String data = Utils.readFile("C:\\Projects\\TELUQ\\src\\Question2\\resources\\part2.txt");
         Sudoku sudoku = new Sudoku(data);
-        sudoku.showMatrix(sudoku.getMatrix());
+        int[][] trasposedMatrix = sudoku.transposeMatrix(sudoku.getMatrix());
+        sudoku.showMatrix(trasposedMatrix);
     }
 }
--- src/Question2/Sudoku.java
+++ src/Question2/Sudoku.java
@@ -1,12 +1,17 @@
 package Question2;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Sudoku {
     public int[][] matrix = new int[9][9];
 
     public Sudoku(String data) throws Exception {
+        int i = 0;
         int x, y, z;
+        List<Integer> list = new ArrayList<>();
         String[] xyz = data.split(" ");
-        
+
         if(xyz.length != 81) throw new Exception("Inavalid Sudoku");
 
         for (String s : xyz) {
@@ -16,6 +21,14 @@ public class Sudoku {
             z = Character.getNumericValue(s.charAt(2));
 
             if(x == 9 || y == 9 || z == 0) throw new Exception("Inavalid Sudoku");
+
+            if(x == i) {
+                if(!list.contains(z)) list.add(z);
+                else throw new Exception("Inavalid Sudoku");
+            } else {
+                list.clear();
+                i++;
+            }
 
             matrix[x][y] = z;
         }
@@ -32,5 +45,15 @@ public class Sudoku {
             }
             System.out.println();
         }
+    }
+
+     public int[][] transposeMatrix(int[][] matrix) {
+        int[][] transposedMatrix = new int[matrix[0].length][matrix.length];
+        for(int i = 0; i < transposedMatrix.length; i++) {
+            for (int j = 0; j < transposedMatrix[i].length; j++) {
+                transposedMatrix[i][j] = matrix[j][i];
+            }
+        }
+        return transposedMatrix;
     }
 }
